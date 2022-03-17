4d37c6584ce8406d43fc25671497ac3f043e2dc1
Alexander Obregon <alexander.obregon47@gmail.com>
2022-03-17 12:44:48 -0700

    Added working signup and login/logout

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,5 +2,5 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password_digest
   has_many :carts
 
-  validates :username, presence: true, uniqueness: true
+
 end
---
+++ client/src/components/Home.js
@@ -0,0 +1,9 @@
+function Home({ user }) {
+    if (user) {
+      return <h1>Welcome, {user.username}! You are logged in! </h1>;
+    } else {
+      return <h1>Not logged in</h1>;
+    }
+  }
+
+  export default Home;
