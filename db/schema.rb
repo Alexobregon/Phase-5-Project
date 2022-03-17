72b3a3a93e0cd8b3ed9503c449910c3678df5c80
Alexander Obregon <alexander.obregon47@gmail.com>
2022-03-17 12:02:09 -0700

    added seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,36 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2022_03_17_183247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_carts_on_product_id"
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "price"
+    t.text "description"
+    t.string "image_url"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "carts", "products"
+  add_foreign_key "carts", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+
+
+puts "Seeding..."
+
+p1 = Product.create(name: "NVIDIA GEFORCE RTX 3090", price: 1499, description: "Cooling System: Fan, Boost Clock Speed: 1.70 GHz, GPU Memory Size: 24 GB", image_url: "https://assets.nvidia.partners/images/png/nvidia-geforce-rtx-3090.png")
+p2 = Product.create(name: "GIGABYTE GeForce RTX 3090 EAGLE 24GB Video Card, GV-N3090EAGLE-24GD", price: 1499, description: "Cooling System: Triple fan, Boost Clock Speed: --, GPU Memory Size: 24 GB", image_url: "https://assets.nvidia.partners/images/png/GV-N3090EAGLE-24GD.png")
+p3 = Product.create(name: "EVGA 24G-P5-3971-KR graphics card", price: 1639, description: "Cooling System: iCX3, Boost Clock Speed: 1695 MHz, GPU Memory Size: 24 GB", image_url: "https://assets.nvidia.partners/images/png/24G-P5-3971-KR.png")
+p4 = Product.create(name: "EVGA - GeForce RTX 3090 XC3 ULTRA GAMING 24GB GDDR6 PCI Express 4.0 Graphics Card", price: 1799, description: "Cooling System: --Boost Clock Speed: --, GPU Memory Size: 24 GB", image_url: "https://assets.nvidia.partners/images/png/24G-P5-3975-KR.png")
+p5 = Product.create(name: "ASUS TUF-RTX3090-24G-GAMING", price: 1874, description: "Cooling System: Triple Fan, Axial-Tech Boost Clock Speed: --, GPU Memory Size: 24 GB", image_url: "https://assets.nvidia.partners/images/png/DE_85935679_100516005_US.png")
+
+
+puts "Done!!"
