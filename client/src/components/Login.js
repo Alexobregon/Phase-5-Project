be00037ca3064b6303431f64fa5698109c88d655
Alexander Obregon <alexander.obregon47@gmail.com>
2022-03-18 18:22:26 -0700

    added css login page and other small css tweaks

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
-import { Button } from 'react-bootstrap'
+import { Button, Nav } from 'react-bootstrap'
+import { Link } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Login({ setUser }) {
   const [username, setUsername] = useState("");
@@ -21,8 +23,68 @@ function Login({ setUser }) {
   }
 
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
+    <div class="container">
+    <div class="row">
+      <div class="col-md-6 offset-md-3">
+        
+        <div class="card my-5">
+
+          <form class="card-body cardbody-color p-lg-5" onSubmit={handleSubmit}>
+
+            <div class="text-center">
+              <img src="https://cdn.pixabay.com/photo/2017/03/21/02/00/user-2160923__340.png" class="img-fluid profile-image-pic img-thumbnail rounded-circle my-3"
+                width="200px" alt="profile"/>
+            </div>
+
+            <div class="mb-3">
+            <input class="form-control" 
+          placeholder="Username"
+          type="text"
+          id="username"
+          autoComplete="off"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+            </div>
+            <div class="mb-3">
+            <input
+          placeholder="Password"
+          class="form-control"
+          type="password"
+          id="password"
+          autoComplete="current-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+            </div> 
+            <div class="text-center">
+              <Button type="submit" class="btn btn-color px-5 mb-5 w-100">Login</Button></div>
+            <div id="emailHelp" class="form-text text-center mb-5 text-dark"> Don't have an Account?
+            <ul class="nav justify-content-center">
+  
+  <li class="nav-item">
+  <Nav>
+  <Nav.Link className="nav-link" as={Link} to="/signup">Create an Account</Nav.Link>
+  </Nav>
+  </li>
+</ul>
+
+            </div>
+          </form>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  )}
+      
+
+  
+
+export default Login;
+
+
+{/* <form onSubmit={handleSubmit}>
         <h1>Login</h1>
         <label htmlFor="username">Username</label>
         <input
@@ -40,10 +102,10 @@ function Login({ setUser }) {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-        <Button type="submit">Login</Button>
-      </form>
-    </div>
-  );
-}
+        <Button variant="success" type="submit">Login</Button>
+        
+      </form> */}
 
-export default Login;
+      // <Nav>
+      //       <Nav.Link className="nav-link" as={Link} to="/signup">Signup</Nav.Link>
+      //       </Nav>
