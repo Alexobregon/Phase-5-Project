e9fe25bf16d2b2cd27c6f99dad41797333f219f5
Alexander Obregon <alexander.obregon47@gmail.com>
2022-03-23 14:10:41 -0700

    added basic avatar stuff in signup/users back end

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,7 +3,12 @@ function Home({ user }) {
 console.log() 
 
     if (user) {
-      return <h1 style={{ color: 'white'}} >Welcome, {user.username}! You are logged in! </h1>;
+      return <div id="homeContainer"> <h1 style={{ color: 'white'}} >Welcome, {user.username}!</h1>
+      <div id="avatarDiv">
+      <img  className="avatarPic" src={user.avatar_url}></img>
+      </div>
+
+      </div>
     } else {
       return <h1 style={{ color: 'white'}} >Not logged in</h1>;
     }
